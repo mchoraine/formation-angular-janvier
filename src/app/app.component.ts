638dc39a617456a6c879c0147e3e02b8a3b9eb76
Martin Choraine <martin.choraine@gmail.com>
2022-01-24 14:21:33 +0100

    tp 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'zenika-shop';
+  title = 'This is my first component';
 }
